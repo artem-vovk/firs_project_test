5ccd30a8a95d88e6ffad4e68afe97e76f99345e9
artem-vovk <artemvovk7777777@gmail.com>
2022-04-12 00:35:21 +0200

    Adding modified file Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// просто внесли изменение в файл
+        System.out.println("Hello");
     }
 }
