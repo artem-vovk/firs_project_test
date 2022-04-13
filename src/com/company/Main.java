13ce3941f7040fd35cc3fa5cc7f937740701aa14
artem-vovk <artemvovk7777777@gmail.com>
2022-04-14 01:35:30 +0200

    commit last changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// просто внесли изменение в файл
-        System.out.println("Hello");
+        System.out.println("Hello again ))");
     }
 }
