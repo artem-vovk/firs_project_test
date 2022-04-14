6ce64604e899bdb8d309cbdadd884bd87ebbdc94
Artem Vovk <artemvovk7777777@gmail.com>
2022-04-15 00:23:01 +0200

    Check connection afetr changes SSH-key

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// просто внесли изменение в файл
         System.out.println("Hello again ))");
+        System.out.println("Создал эту запись для того чтобы проверить подключения ГитХаба после переподключения SSH-ключа ))");
     }
 }
